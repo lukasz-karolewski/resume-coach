79113f264fd0b67dcd2203bc49201172a5f00f9e
Karolewski <lkarolewski@lkarolew04scl.corp.homestore.net>
2019-01-11 12:32:39 -0800

    added input verification and automatically creates volume if missing

--- resume/build_pdf.sh
+++ resume/build_pdf.sh
@@ -1,9 +1,20 @@
+#!/bin/bash
+VOLUME_NAME=miktex
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <tex filename>"
+    exit 1
+fi
+
+if ! [[ $(docker volume ls --filter=name=$VOLUME_NAME --format='{{.Name}}') ]]; then 
+  echo "$VOLUME_NAME not found, creating"
+  docker volume create --name $VOLUME_NAME
+fi
+
 docker run -ti \
   -v miktex:/miktex/.miktex \
   -v `pwd`:/miktex/work \
   miktex/miktex \
   pdflatex $1
 
-rm *.log
-rm *.aux
-rm *.out
+rm *.log *.aux *.out
