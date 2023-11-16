d7a8aa6380ece90e90e4549528c3f4606bfd5ccc
Lukasz Karolewski <lkarolewski@gmail.com>
2023-11-16 07:23:49 +0000

    temporary workaround for signout

--- homepage/app/components/user-button.tsx
+++ homepage/app/components/user-button.tsx
@@ -13,37 +13,54 @@ import { SignIn, SignOut } from "./auth-components";
 export default async function UserButton() {
   const session = await auth();
   if (!session?.user) return <SignIn />;
-
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="relative w-8 h-8 rounded-full">
-          <Avatar className="w-8 h-8">
-            {session.user.image && (
-              <AvatarImage
-                src={session.user.image}
-                alt={session.user.name ?? ""}
-              />
-            )}
-            <AvatarFallback>{session.user.email}</AvatarFallback>
-          </Avatar>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56 bg-white" align="end" forceMount>
-        <DropdownMenuLabel className="font-normal">
-          <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">
-              {session.user.name}
-            </p>
-            <p className="text-xs leading-none text-muted-foreground">
-              {session.user.email}
-            </p>
-          </div>
-        </DropdownMenuLabel>
-        <DropdownMenuItem>
-          <SignOut />
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  );
+  else
+    return (
+      <div className="flex items-center">
+        <Avatar className="w-8 h-8">
+          {session.user.image && (
+            <AvatarImage
+              src={session.user.image}
+              alt={session.user.name ?? ""}
+            />
+          )}
+          <AvatarFallback>{session.user.email}</AvatarFallback>
+        </Avatar>
+        <SignOut />
+      </div>
+    );
+  // TODO below fails with "Form submission cancelled because the form is not connected error?"
+  // return (
+  //   <DropdownMenu>
+  //     <DropdownMenuTrigger asChild>
+  //       <Button variant="ghost" className="relative w-8 h-8 rounded-full">
+  //         <Avatar className="w-8 h-8">
+  //           {session.user.image && (
+  //             <AvatarImage
+  //               src={session.user.image}
+  //               alt={session.user.name ?? ""}
+  //             />
+  //           )}
+  //           <AvatarFallback>{session.user.email}</AvatarFallback>
+  //         </Avatar>
+  //       </Button>
+  //     </DropdownMenuTrigger>
+  //     <DropdownMenuContent className="w-56 bg-white" align="end" forceMount>
+  //       <DropdownMenuLabel className="font-normal">
+  //         <div className="flex flex-col space-y-1">
+  //           <p className="text-sm font-medium leading-none">
+  //             {session.user.name}
+  //           </p>
+  //           <p className="text-xs leading-none text-muted-foreground">
+  //             {session.user.email}
+  //           </p>
+  //         </div>
+  //       </DropdownMenuLabel>
+  //       <DropdownMenuItem>
+  //         <div>
+  //           <SignOut />
+  //         </div>
+  //       </DropdownMenuItem>
+  //     </DropdownMenuContent>
+  //   </DropdownMenu>
+  // );
 }
