22cd6ea5eaa1e6c23e08f81ae7d454208b6e2097
Lukasz Karolewski <lkarolewski@gmail.com>
2024-01-08 22:09:52 -0800

    tmp link and h1 style

--- src/components/ui/top-nav.tsx
+++ src/components/ui/top-nav.tsx
@@ -1,12 +1,17 @@
-import UserButton from "./user-button";
+import Link from "next/link";
 
+import UserButton from "./user-button";
 const TopNav: React.FC = async () => {
   return (
     <nav className=" print:hidden">
       <div className="mx-auto flex max-w-4xl items-center gap-8  p-4">
-        <h1>Resume Coach</h1>
+        <Link href="/">
+          <h1>Resume Coach</h1>
+        </Link>
 
-        <div className="grow"></div>
+        <div className="grow">
+          <Link href="/resume">static resume</Link>
+        </div>
 
         <UserButton />
       </div>
