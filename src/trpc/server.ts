04023e9fc6b17249ea5c27217d3d38d30f052af2
Lukasz Karolewski <lkarolewski@gmail.com>
2024-01-24 18:03:55 +0000

    bump libs

--- src/trpc/server.ts
+++ src/trpc/server.ts
@@ -1,11 +1,7 @@
 import "server-only";
 
-import {
-  createTRPCProxyClient,
-  loggerLink,
-  TRPCClientError,
-} from "@trpc/client";
-import { callProcedure } from "@trpc/server";
+import { createTRPCClient, loggerLink, TRPCClientError } from "@trpc/client";
+import { callTRPCProcedure } from "@trpc/server";
 import { observable } from "@trpc/server/observable";
 import { type TRPCErrorResponse } from "@trpc/server/rpc";
 import { cookies } from "next/headers";
@@ -29,7 +25,7 @@ const createContext = cache(() => {
   });
 });
 
-export const api = createTRPCProxyClient<typeof appRouter>({
+export const api = createTRPCClient<typeof appRouter>({
   transformer,
   links: [
     loggerLink({
@@ -46,10 +42,10 @@ export const api = createTRPCProxyClient<typeof appRouter>({
         observable((observer) => {
           createContext()
             .then((ctx) => {
-              return callProcedure({
+              return callTRPCProcedure({
                 procedures: appRouter._def.procedures,
                 path: op.path,
-                rawInput: op.input,
+                getRawInput: async () => op.input,
                 ctx,
                 type: op.type,
               });
