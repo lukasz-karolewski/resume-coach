8d7009bcec127fe7f287d2c8453375820b5b9378
Lukasz Karolewski <lkarolewski@gmail.com>
2024-03-04 06:51:16 +0000

    added redis and pg to dev setup

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,13 @@
+from celery_app.tasks import import_job
+from dotenv import load_dotenv
 from fastapi import FastAPI
 
-from tasks import import_job
-
+load_dotenv()  # take environment variables from .env.
 app = FastAPI()
 
+
 @app.post("/import-job")
-async def trigger_import_job(url: str):
-    #import_job.delay(url)
-    return {"message": "Import job triggered successfully"}
+async def trigger_import_job(request_data: dict):
+    url = request_data.get("url")
+    import_job.delay(url)
+    return {"message": f"Import job triggered successfully {url}"}
