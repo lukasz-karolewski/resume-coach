e03e394db5aec05edabbafa4a2dd8d7a5423571f
Lukasz Karolewski <lkarolewski@gmail.com>
2025-11-13 00:24:57 -0800

    fixing build, bumping libs

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,4 +1,9 @@
-export { auth as middleware } from "~/auth";
+import NextAuth from "next-auth";
+import authConfig from "./auth-config";
+
+export const { auth } = NextAuth(authConfig);
+
+export default auth;
 
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
