0d1b1bd19105668be5ad8bba37377205953d196c
Lukasz Karolewski <lkarolewski@gmail.com>
2026-03-24 02:55:27 +0000

    feat: initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,13 @@
 import clsx from "clsx";
 import type { Metadata } from "next";
-import { Noto_Serif } from "next/font/google";
+import { Noto_Serif, Playfair_Display } from "next/font/google";
 import { cookies } from "next/headers";
 import NiceModalProviderWrapper from "~/components/providers";
 import { TRPCReactProvider } from "~/trpc/react";
 import "./styles.css";
+import { cn } from "~/lib/utils";
+
+const playfairDisplay = Playfair_Display({subsets:['latin'],variable:'--font-serif'});
 
 const font = Noto_Serif({ subsets: ["latin"] });
 
@@ -19,7 +22,7 @@ interface RootLayoutProps {
 
 export default async function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-serif", playfairDisplay.variable)}>
       <body className={clsx(font.className, "min-w-96 grid min-h-screen")}>
         <TRPCReactProvider cookies={(await cookies()).toString()}>
           <NiceModalProviderWrapper>{children}</NiceModalProviderWrapper>
